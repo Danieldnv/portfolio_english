02cf0283253a8b0ac60ef0a26be18e9d8456371a
Daniel Vital <134316491+Danieldnv@users.noreply.github.com>
2025-01-06 16:01:58 -0300

    Update animated_plot_portfolio.R

--- codes/animated_plot_portfolio.R
+++ codes/animated_plot_portfolio.R
@@ -6,23 +6,29 @@ library(gganimate)
 library(gifski)
 
 
-############### Colecting default data from the central bank of Brazil #####################
+############### Colecting default data using the central bank of Brazil's API #####################
 serie_bcb2 <- c(individuals = 21083, legal_entities = 21084)
 initial_date<-'2011-03-01'
 
-default_bcb2<- gbcbd_get_series(id=serie_bcb2, first.date = data_inicial,
+default_bcb2<- gbcbd_get_series(id=serie_bcb2, first.date = initial_date,
                                       last.date = Sys.Date(), use.memoise = F)
 
+
 ############### Building a data frame from the data I just obtained #########################
+
+# PJ includes only the default rate of legal entities
 PJ<- default_bcb2%>%
   filter(series.name == 'legal_entities')%>%
   select(value)
-
+# PF includes only the default rate of individuals
 PF <- default_bcb2%>%
   filter(series.name == 'individuals')%>%
   select(value)
 
-df_default <- data.frame(inadimplencia_bcb2$ref.date, PF, PJ)
+#Separating the dates from default_bcb2, so I can use it as the information for my 'x' axis and including PF and PJ in the same dataframe.
+df_default <- data.frame(default_bcb2$ref.date, PF, PJ)
+
+#Naming the columns for a better visualization
 colnames(df_default)= c('Year', 'Individuals', 'Legal_entities')
 
 ########################### Plotting the graph and animating it #######################
@@ -49,9 +55,7 @@ animated_plot <- estatico +
   transition_reveal(Year) +    
   ease_aes('linear')        
 
-# agora, usando 'animate' para animar 'animated_plot', criado anteriormente.
-#Now, I am going to use 'animate' to animate 'animated_plot'
-
+#Next, I will employ the 'animate' function to animate the plot
 
 ap_better <- animate(animated_plot, nframes = 400, fps = 50,width = 600, height = 400, 
                      renderer = gifski_renderer())
